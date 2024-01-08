df3df6241cdb84f52f8297ec8d1435cf3c047db1
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-08 17:21:53 +1100

    created Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
+import styles from "./Header.module.scss";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.container}>
+      <img className={styles.logo} src="../src/assets/logo.png" />
+      <h1 className={styles.heading}>Pokédex</h1>
+    </div>
+  );
 };
 
 export default Header;
